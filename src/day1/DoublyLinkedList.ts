d64e4104c140b2f0a069061a9a85332135016229
Ibrahim Taofeek <taofeekibrahimope@gmail.com>
2024-06-20 08:23:28 +0100

    doubly linked list

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,53 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+};
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
-    prepend(item: T): void {
+    prepend(item: T): void {}
 
-}
     insertAt(item: T, idx: number): void {
+        if (idx > this.length) {
+            throw new Error("oh no");
+        } else if (idx === this.length) {
+            this.append(item);
+        } else if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
 
-}
-    append(item: T): void {
+        this.length++;
+        let curr = this.head;
 
-}
-    remove(item: T): T | undefined {
+        for (let i = 0; curr && i < idx; ++i) {
+            curr = curr.next;
+        }
 
-}
-    get(idx: number): T | undefined {
+        curr = curr as Node<T>;
+        const node = { value: item } as Node<T>;
 
-}
-    removeAt(idx: number): T | undefined {
+        node.next = curr;
+        node.prev = curr.prev;
+        curr.prev = node;
 
+        if (node.prev) {
+            curr.prev.next = curr;
+        }
+    }
+
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
-}
+
+
